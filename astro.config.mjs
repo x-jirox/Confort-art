15900ed5aad92a1518b3a3f0f65e1ffc3b7f800b
x-jirox <correalenin045gmail.com>
2025-03-25 02:03:39 -0500

    new confort art

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+      },
+    output: 'server',
+    adapter: vercel(),
+
+});
